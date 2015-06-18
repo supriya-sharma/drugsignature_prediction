969fd7dd07efb23873bbcebad7d2496c50c8aae0
supriya sharma <supriya7@scc-cb3.scc.bu.edu>
2015-06-18 16:09:17 -0400

    fixed topCor strsplit error

--- methyl_vpa_supriya.R
+++ methyl_vpa_supriya.R
@@ -144,7 +144,7 @@ topGenes_vpa6h_2 <- topTable(fit2, coef=3,number=nTop)
 #  associate methylation sites with genes vpa2
 vpa2h <- topGenes_vpa2h_2[topGenes_vpa2h_2[,5]<0.05,]
 vpa2_id <- rownames(vpa2h)
-vpa2_id <- vpa2h$ID ### Evan's edit
+
 vpa2_gene <- NULL
 for (i in 1:length(vpa2_id)){
   vpa2_gene <- c(vpa2_gene, strsplit(vpa2_id[i],split="_")[[1]][4])
@@ -190,7 +190,7 @@ topGenes_vpa6h_2 <- topTable(fit2, coef=3,number=nTop)
 vpa2h <- topGenes_vpa2h_2[topGenes_vpa2h_2[,5]<0.05,]
 
 vpa2_id <- rownames(vpa2h)
-vpa2_id <- vpa2h$ID
+
 vpa2_gene <- NULL
 for (i in 1:length(vpa2_id)){
 vpa2_gene <- c(vpa2_gene, strsplit(vpa2_id[i],split="_")[[1]][4])
@@ -198,7 +198,7 @@ vpa2_gene <- c(vpa2_gene, strsplit(vpa2_id[i],split="_")[[1]][4])
 vpa2_gene_uniq <- unique(vpa2_gene)
 keep2 = (vpa2_gene != "NONE") & (!duplicated(vpa2_gene))   ## Evan remove diuplicated probes and controle probes ("NONE")
                            
-topGenes_vpa2h_2_keep = topGenes_vpa2h_2[keep2,][1:5000,]  
+topGenes_vpa2h_2_keep = topGenes_vpa2h_2[keep2,]  
 
 #write.csv(topGenes_vpa2h_2, file="VPA_diff_Me_Unique_GeneList_5000.csv")
                            
@@ -206,7 +206,7 @@ topGenes_vpa2h_2_keep = topGenes_vpa2h_2[keep2,][1:5000,]
 vpa6h <- topGenes_vpa6h_2[topGenes_vpa6h_2[,5]<0.05,]
                           
 vpa6_id <- rownames(vpa6h)
-vpa6_id <- vpa6h$ID
+
 vpa6_gene <- NULL
 for (i in 1:length(vpa6_id)){
 vpa6_gene <- c(vpa6_gene, strsplit(vpa6_id[i],split="_")[[1]][4])
@@ -214,11 +214,10 @@ vpa6_gene <- c(vpa6_gene, strsplit(vpa6_id[i],split="_")[[1]][4])
 vpa6_gene_uniq <- unique(vpa6_gene)
 keep6 = (vpa6_gene != "NONE") & (!duplicated(vpa6_gene))   ## Evan remove diuplicated probes and controle probes ("NONE")
                            
-topGenes_vpa6h_2_keep = topGenes_vpa6h_2[keep6,][1:5000,] 
+topGenes_vpa6h_2_keep = topGenes_vpa6h_2[keep6,] 
 
 write.csv(topGenes_vpa6h_2_keep, file="VPA_diff_Me_Unique_GeneList_5000.csv")
                            
-##VPA_2h
 
 topGenes_unique <- read.csv("correlation_unique_500_geneList.csv", header = TRUE)
                            
@@ -226,10 +225,10 @@ topGenes_unique <- read.csv("correlation_unique_500_geneList.csv", header = TRUE
 #get the probe names (from topGenes_vpa6h_2_keep) for the genes in the correlated list
 
 selected=NULL
-for (i in topGenes_vpa6h_2_keep$ID){
-selected <- c(selected, strsplit(i,split="_")[[1]][4]%in%topGenes_unique$genes)
+for (i in rownames(topGenes_vpa6h_2_keep)){
+selected <- c(selected, strsplit(i,split= "_")[[1]][4]%in%topGenes_unique$genes)
 }
-topCor=topGenes_vpa6h_2_keep$ID[selected]
+topCor=rownames(topGenes_vpa6h_2_keep)[selected]
                            
 S_matrix <- -fit2$coefficients[rownames(fit2$coefficients)%in%topCor,2]
 B_vector <- fit2$coefficients[rownames(fit2$coefficients)%in%topCor,1]+fit2$coefficients[rownames(fit2$coefficients)%in%topCor,2]
@@ -241,7 +240,7 @@ geneList_vpa6h <- rownames(topGenes_vpa6h_2_keep)
                            
 S_matrix <- -fit2$coefficients[rownames(fit2$coefficients)%in%topCor,3]
 B_vector <- fit2$coefficients[rownames(fit2$coefficients)%in%topCor,1]+fit2$coefficients[rownames(fit2$coefficients)%in%topCor,3]
-Pi_matrix <- rep(0.95,length(topCor))
+Pi_matrix <- rep(0.95,length(S_matrix))
                            
 #TCGA
                          
@@ -322,7 +321,7 @@ mcmc.chain <- assign.mcmc(Y = testData_sub_110p5_logit, Bg = B_vector, X = S_mat
 mcmc.pos.mean4 <- assign.summary(test=mcmc.chain, burn_in=1000, iter=2000, adaptive_B=T, adaptive_S=T,mixture_beta=T)
 vpa_pa <- mcmc.pos.mean4$kappa_pos
 row.names(vpa_pa) <- names(testData_sub_TCGA)
-write.csv(vpa_pa, file="methylation_TCGA_all_200genes.csv") # csv file for all TCGA tumor-normal samples
+write.csv(vpa_pa, file="methylation_TCGA_all_500_topCor.csv") # csv file for all TCGA tumor-normal samples
 
 
 # plots and tables
@@ -365,8 +364,8 @@ pdf("methylation_320p2_50_vpa_6h.pdf")
 boxplot(mcmc.pos.mean4$kappa ~ label,ylab="vpa signature",main="320p2_50_vpa_6h_methylation_signature.pdf")
 dev.off()                           
 
-pdf("methylation$$_Unique+combat_tcga_vpa6h_500.pdf")
-boxplot(mcmc.pos.mean4$kappa ~ label, ylab="vpa signature",col=c("darkgreen","red"),main="TCGA_combat_vpa_500_6h_methylation_Unique+Combat.pdf")
+pdf("methylation$$_tcga_vpa6h_500_topCor.pdf")
+boxplot(mcmc.pos.mean4$kappa ~ label, ylab="vpa signature",col=c("darkgreen","red"),main="TCGA_vpa_6h_methylation_topCor.pdf")
 dev.off()  
 
 
@@ -386,155 +385,8 @@ axis(1.,at=c(1,2,4,5,6,7,8),labels=c("Normal","Tumor","Basal","Her2","Lum-A","Lu
 dev.off()
 
 ############################################################
-# Ying's gene expression analysis
 
-library(sva)
 
-expr <- read.table("finalMerged.txt",row.names="external_gene_id",header=T)
-expr2 <- expr[complete.cases(expr),]
-
- ### pca before ComBat
-pca <- prcomp(t(expr2), retx=T, center=T, scale=T)
-pc <- pca$x
-                           
-tissue <- as.character(c(rep("00",36),sapply(names(expr2)[37:283],function(x){substr(x,14,15)})))
-subBatch <- as.character(c(rep("cellLine",36),sapply(names(expr2)[37:283],function(x){substr(x,22,25)})))
-                           
-plot(pc[,1], pc[,2], col=as.factor(tissue))
-plot(pc[,1], pc[,2], col=as.factor(subBatch))
-                           
-### ComBat
-batch <- c(rep("cellLine",36),rep("tcga",247))
-expr_combat <- ComBat(dat=expr2, batch, mod=NULL,numCovs=NULL, par.prior=TRUE,prior.plots=FALSE)
-write.table(expr_combat,file="exprFinal_v2_combat.txt",quote=F,sep="\t")                           
-### pca after ComBat
-pca <- prcomp(t(expr_combat), retx=T, center=T, scale=T)
-pc <- pca$x
-                           
-tissue <- as.character(c(rep("00",36),sapply(names(expr_combat)[37:283],function(x){substr(x,14,15)})))
-subBatch <- as.character(c(rep("cellLine",36),sapply(names(expr_combat)[37:283],function(x){substr(x,22,25)})))
-                           
-plot(pc[,1], pc[,2], col=as.factor(tissue))
-plot(pc[,1], pc[,2], col=as.factor(subBatch))
-                           
-# ### subset cell line data to the vpa treatment and ctr using the expr_combat
-# cellline <- expr_combat[,1:36]
-# vpa.cellline <- sort(names(cellline)[c(grep("vpa",names(cellline)),grep("ctr",names(cellline)))])[-c(3,4,11,12,19)]
-# vpa <- cellline[,vpa.cellline]
- 
-# vpa.matrix <- as.matrix(vpa)
-# group <- rep(c(1,0),length=16)
-# pair <- as.factor(rep(1:8,each=2,length=16))
-                           # 
-# #ctr.idx <- seq(1,16,by=2)
-# #vpa.idx <- seq(2,16,by=2)
-# #t.test(vpa.matrix[1,ctr.idx],vpa.matrix[1,vpa.idx],paired=T)
-  
-# ### select significant genes using expr_combat
- # result <- matrix(nrow=nrow(vpa.matrix),ncol=4)
-# colnames(result) <- c("est_beta0","est_beta1","tstat", "pvalue")
-# rownames(result) <- row.names(vpa)
-# for (i in 1:nrow(vpa.matrix)){
-#   if(i%%100==0){print(i)}
-#   lm1 <- lm(vpa.matrix[i,] ~ group + pair)
-#   result[i,] <- c(summary(lm1)$coef[1,1],summary(lm1)$coef[2,c(1,3,4)])
-# }
-
-# topGenes <- result[order(result[,4]),]
-# fdr <- p.adjust(topGenes[,4],method="fdr")
-# topGenes <- cbind(topGenes,fdr)
-                           
-### subset cell line data to the vpa treatment and ctr using the expr2
-cellline <- expr2[,1:36]
-vpa.cellline <- sort(names(cellline)[c(grep("vpa",names(cellline)),grep("ctr",names(cellline)))])[-c(3,4,11,12,19)]
-vpa <- cellline[,vpa.cellline]
-                           
-vpa.matrix <- as.matrix(vpa)
-group <- rep(c(1,0),length=16)
-                           pair <- as.factor(rep(1:8,each=2,length=16))
-                           
-                           #ctr.idx <- seq(1,16,by=2)
-                           #vpa.idx <- seq(2,16,by=2)
-                           #t.test(vpa.matrix[1,ctr.idx],vpa.matrix[1,vpa.idx],paired=T)
-                           
-                           # ### select significant genes using expr2
-                           # result <- matrix(nrow=nrow(vpa.matrix),ncol=4)
-                           # colnames(result) <- c("est_beta0","est_beta1","tstat", "pvalue")
-                           # rownames(result) <- row.names(vpa)
-                           # for (i in 1:nrow(vpa.matrix)){
-                           #   #if(i%%100==0){print(i)}
-                           #   lm1 <- lm(vpa.matrix[i,] ~ group + pair)
-                           #   result[i,] <- c(summary(lm1)$coef[1,1],summary(lm1)$coef[2,c(1,3,4)])
-                           # }
-                           # 
-                           # topGenes <- result[order(result[,4]),]
-                           # fdr <- p.adjust(topGenes[,4],method="fdr")
-                           # topGenes <- cbind(topGenes,fdr)
-                           
-# alternative limma
-library(limma)
-design <-  model.matrix(~group+pair)
-fit <- lmFit(vpa.matrix,design)
-fit <- eBayes(fit)
-                           
-topGenes_vpa <-topTable(fit,coef=2,number=nrow(vpa.matrix))
-### the gene selected from the combat adjusted data is the same as the combat unadjusted data (est_beta is different, but the t-statistic and p-values are the same.)
-                           
-### ASSIGN
-library(ASSIGN)
-nTop <- 200
-geneList <- rownames(topGenes)[1:nTop]
-testData_sub <- expr_combat[geneList,37:283]
-#testData_sub <- expr2[geneList,37:283]
-B_vector <- topGenes[1:nTop,1]
-S_matrix <- topGenes[1:nTop,2]
-Pi_matrix <- rep(0.95,nTop)
-                           
-##limma
-geneList <- topGenes_vpa$ID[1:nTop]
-#testData_sub <- expr_combat[geneList,37:283]
-testData_sub <- expr2[geneList,37:283]
-B_vector <- fit$coef[geneList,1]
-S_matrix <- fit$coef[geneList,2]
-Pi_matrix <- rep(0.95,nTop)
-                           
-                           
-                           #test1: adaptive_B=F, adaptive_S=F, mixture_beta=F
-                           mcmc.chain <- assign.mcmc(Y = testData_sub, Bg = B_vector, X = S_matrix, Delta_prior_p = Pi_matrix, iter=2000, adaptive_B=F, adaptive_S=F, mixture_beta=F, p_beta = 0.5)
-                           mcmc.pos.mean1 <- assign.summary(test=mcmc.chain, burn_in=1000, iter=2000, adaptive_B=F, adaptive_S=F,mixture_beta=F)
-                           
-                           #test2: adaptive_B=T, adaptive_S=F, mixture_beta=F
-                           mcmc.chain <- assign.mcmc(Y = testData_sub, Bg = B_vector, X = S_matrix, Delta_prior_p = Pi_matrix, iter=2000, adaptive_B=T, adaptive_S=F, mixture_beta=F, p_beta = 0.5)
-                           mcmc.pos.mean2 <- assign.summary(test=mcmc.chain, burn_in=1000, iter=2000, adaptive_B=T, adaptive_S=F,mixture_beta=F)
-                           
-                           #test3: adaptive_B=T, adaptive_S=T, mixture_beta=F
-                           mcmc.chain <- assign.mcmc(Y = testData_sub, Bg = B_vector, X = S_matrix, Delta_prior_p = Pi_matrix, iter=2000, adaptive_B=T, adaptive_S=T, mixture_beta=F, p_beta = 0.5)
-                           mcmc.pos.mean3 <- assign.summary(test=mcmc.chain, burn_in=1000, iter=2000, adaptive_B=T, adaptive_S=T,mixture_beta=F)
-                           
-                           #test4: adaptive_B=T, adaptive_S=T, mixture_beta=T
-                           mcmc.chain <- assign.mcmc(Y = testData_sub, Bg = B_vector, X = S_matrix, Delta_prior_p = Pi_matrix, iter=2000, adaptive_B=T, adaptive_S=T, mixture_beta=T, p_beta = 0.5)
-                           mcmc.pos.mean4 <- assign.summary(test=mcmc.chain, burn_in=1000, iter=2000, adaptive_B=T, adaptive_S=T,mixture_beta=T)
-                           
-                           # plots and tables
-                           label <- as.factor(c(rep("normal",32),rep("tumor",215)))
-                           pdf("ComBat_test.pdf")
-                           par(mfrow=c(2,2))
-                           boxplot(mcmc.pos.mean1$beta ~ label,ylab="vpa signature",main="test1")
-                           boxplot(mcmc.pos.mean2$beta ~ label,ylab="vpa signature",main="test2")
-                           boxplot(mcmc.pos.mean3$beta ~ label,ylab="vpa signature",main="test3")
-                           boxplot(mcmc.pos.mean4$kappa ~ label,ylab="vpa signature",main="test4")
-                           dev.off()
-                           
-                           pa <- matrix(mcmc.pos.mean4$kappa,nrow=ncol(testData_sub),1);rownames(pa) <- names(testData_sub)
-                           pa1 <- pa[order(rownames(pa)),1,drop=F]
-                           pa2 <- pa1[c(grep("11A",rownames(pa1))-1, grep("11B",rownames(pa1))-1,grep("11A",rownames(pa1)), grep("11B",rownames(pa1))),1,drop=F]
-                           dim(pa2) <- c(32,2)
-                           rownames(pa2) <- sapply(rownames(pa1)[1:32],function(x){substr(x,1,12)})
-                           colnames(pa2) <- c("tumor", "normal")
-                           
-                           write.csv(pa2,file="Combat_test4.csv")
-                           
-#######################
 ## Supriya's analysis-gene selection for gene expression by LIMMA
 
 library(sva)
@@ -612,24 +464,51 @@ pdf("expression_TCGA_1000_supriya.pdf")
 boxplot(mcmc.pos.mean4$kappa ~ label,ylab="vpa signature",main="TCGA_combat_vpa_1000_expression.pdf")
 dev.off()  
                            
+#############################################
 
-#to extract values for specific genes using their names
-expr <- read.table("finalMerged.txt",row.names="external_gene_id",header=T)
-expr(rownames(expr) == "CREBBP"")
- 
-       
- %in%c("CREBBP", "MAST3")
+## gene expression analysis with top correlated genes
 
        
-       # search for overlapping gens
-a <- c("WDR60","CREBBP", "MAST3")
-b <- read.table("finalMerged.txt",row.names="external_gene_id",header=T) 
-x <- match(a,b)
-x
-       a %in% b
-       
-match(x="CREBBP", table="finalMerged.txt")
-       
-       x %in% table       
-       
-       
+###########
+### ASSIGN
+library(ASSIGN, "/usr2/faculty/wej/R/x86_64-unknown-linux-gnu-library/2.15")
+           
+topGenes_unique <- read.csv("correlation_unique_500_geneList.csv", header = TRUE)
+                           
+### Link exp/meth neg corr genes with limma result above:
+#get the probe names (from topGenes_vpa6h_2_keep) for the genes in the correlated list
+
+selected=NULL
+for (i in rownames(topGenes_vpa6h_2_keep)){
+selected <- c(selected, strsplit(i,split= "_")[[1]][4]%in%topGenes_unique$genes)
+}
+topCor=rownames(topGenes_vpa6h_2_keep)[selected]
+                           
+                                                 
+#TCGA
+                         
+###
+geneList_vpa <- rownames(topGenes_vpa6h_2_keep)
+S_matrix <- -fit2$coefficients[rownames(fit2$coefficients)%in%topCor,3]
+B_vector <- fit2$coefficients[rownames(fit2$coefficients)%in%topCor,1]+fit2$coefficients[rownames(fit2$coefficients)%in%topCor,3]
+Pi_matrix <- rep(0.95,length(S_matrix))
+                           
+                           
+#TCGA
+
+testData_TCGA_sub <- expr_combat[topCor,37:283]
+                           
+#test4: adaptive_B=T, adaptive_S=T, mixture_beta=T
+mcmc.chain <- assign.mcmc(Y = testData_TCGA_sub , Bg = B_vector, X = S_matrix, Delta_prior_p = Pi_matrix, iter=2000, adaptive_B=T, adaptive_S=T, mixture_beta=T, p_beta = 0.5)
+mcmc.pos.mean4 <- assign.summary(test=mcmc.chain, burn_in=1000, iter=2000, adaptive_B=T, adaptive_S=T,mixture_beta=T)
+vpa_pa <- mcmc.pos.mean4$beta_pos
+                           
+row.names(vpa_pa) <- names(testData_sub_TCGA)
+write.csv(vpa_pa, file="expression_TCGA_all_supriya_topCor.csv") # csv file for all TCGA tumor-normal samples
+                           
+# plots and tables
+label <- as.factor(c(rep("normal",32),rep("tumor",215)))
+                           
+pdf("expression_TCGA_1000_supriya.pdf")
+boxplot(mcmc.pos.mean4$kappa ~ label,ylab="vpa signature",main="TCGA_combat_vpa_1000_expression.pdf")
+dev.off() 
